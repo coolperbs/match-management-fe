4315a282b1501766b63908292270d273cb673f23
Coolperbs <672735341@qq.com>
2024-12-09 23:42:28 +0800

    feat: 开发完成

--- src/match.d.ts
+++ src/match.d.ts
@@ -1,41 +1,3 @@
-type User = {
-  // 用户信息
-  name: string
-  phone: string
-  email: string
-  idCard: string
-  age: number
-  gender: string
-  address: string
-  school: string
-  grade: string
-  class: string
-}
-type Team = {
-  name: string
-  userList: Array<User>
-}
-type ChampionshipBasicInfo = {
-  // 比赛基本信息
-  name: string
-  address: string
-  fromDate: Date
-  endDate: Date
-  registrationFromDate: Date
-  registrationEndDate: Date
-  concatUserList: Array<User>
-  ProjectGroupList: Array<ProjectGroup>
-  status: '筹备中' | '待审核' | '已通过'
-}
-type ChampionshipItem = {
-  // 比赛项目组(青年组，少年组，老年组)
-  name: string
-  alias: string // 代号
-  minAge: number
-  maxAge: number
-  registrationType: '实名' | '身份证' | '电话'
-  projectList: Array<Project>
-}
 type ProjectType =
   | '男单'
   | '女单'
@@ -47,71 +9,200 @@ type ProjectType =
   | '男团'
   | '女团'
   | '混团'
+
 type MatchType = '3局2胜' | '5局3胜' | '7局4胜' | '9局5胜' | '1局胜'
 type PointType = '31' | '21'
+type ChampionshipType = '羽毛球' | '乒乓球' | '网球'
+type PlayerType = 'person' | 'team'
+
+type User = {
+  // 用户信息
+  id: number
+  name: string
+  phone: string
+  email: string
+  idCard: string
+  age: number
+  gender: string
+  address: string
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
+}
+
+type Team = {
+  id: number
+  name: string
+  userList: Array<User>
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
+}
+
+type Championship = {
+  // 赛事
+  // 比赛基本信息
+  id: number
+  name: string
+  address: string
+  fromDate: Date
+  endDate: Date
+  type: ChampionshipType
+  registrationFromDate: Date
+  registrationEndDate: Date
+  concatUserList: Array<User>
+  ProjectGroupList: Array<ProjectGroup>
+  status: '筹备中' | '待审核' | '已通过'
+  concatUserList: Array<User> // 联系人
+  adImage: string // 广告图
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
+}
+
+type Category = {
+  // 赛事分组
+  // 比赛项目组(青年组，少年组，老年组)
+  id: number
+  name: string
+  alias: string // 代号
+  minAge: number
+  maxAge: number
+  registrationType: '实名' | '身份证' | '电话'
+  projectList: Array<Project>
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
+}
+
 type Project = {
   // 比赛项目(男单/女单)
+  id: number
   name: string
   type: ProjectType
   fee: number
-  minPersionCount: number
-  maxPersonCount: number
+  minPlayerCount: number
+  maxPlayerCount: number
   stageList: Array<ProjectStage> // 阶段
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
 }
+
 type Stage = {
   // 比赛阶段(小组赛，淘汰赛)
+  id: number
+  type: '循环赛' | '淘汰赛' // round-robin | knockout 赛制类型
   name: string
-  personCount: number
-  groupCount: number
-  groupWinnerCount: number
-  subStageList: Array<SubStage>
-}
-type SubStage = {
-  type: '循环赛' | '淘汰赛' // round-robin | knockout
   playerCount: number
+  playerType: PlayerType // 参赛人员类型
   // groupCount: number
   // winnerCount: number
-  matchType: MatchType
-  pointType: PointType
+  matchType: MatchType // 局赛制
+  pointType: PointType // 分赛制
   groupList: Array<Group>
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
 }
-type knockoutSubStage = {
+
+type knockoutStage = {
   // 淘汰赛
-  winnerCount: number
-} & SubStage
-type roundRobinSubStage = {
+  winnerCount: number // 晋级数量
+} & Stage
+
+type roundRobinStage = {
   // 循环赛
-  nameType: 'ABCD' | '1234' | '一二三四'
+  nameType: 'ABCD' | '1234' | '一二三四' // 命名方式
   groupCount: number
   groupWinnerCount: number
-  loopCount: '单循环' | '双循环'
-  organizationType: '逆时针' | '顺时针' | '贝尔格'
-} & SubStage
+  loopCount: '单循环' | '双循环' // 循环次数
+  organizationType: '逆时针' | '顺时针' | '贝尔格' // 编排方式
+} & Stage
+
 type Group = {
   // 比赛阶段小组
+  id: number
   name: string
   playerList: Array<User>
   matchList: Array<Match>
+  teamList: Array<Team>
+  teamMatchList: Array<TeamMatch>
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
 }
+
+type Lottery = {
+  // 抽签
+  id: number
+  player: Array<User>
+  team: Team
+  name: string
+}
+
 type Match = {
   // 对决
+  id: number
   groupLoopIndex: number
   type: ProjectType
-  player1: User | Team | null
-  player2: User | Team | null
+  // player1: Lottery | null
+  // player2: Lottery | null
+  // player3: Lottery | null
+  // player4: Lottery | null
   isTeamMatch: boolean
-  player1Point: number
-  player2Point: number
+  homePlayer: Lottery // 主队
+  awayPlayer: Lottery // 客队
+  homePlayerPoint: number // 主队得分
+  awayPlayerPoint: number // 客队得分
   matchType: MatchType
   pointType: PointType
   roundList: Array<Round>
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
 }
+
 type TeamMatch = {
+  id: number
   teamMatchList: Array<Match>
 } & Match
+
 type Round = {
   // 对决局
+  id: number
+  // matchId: number
+  left: Lottery
+  right: Lottery
+  winner: Lottery
+  leftPoint: number
+  rightPoint: number
+  pointProcess: Array<Point>
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
 }
+
 type Point = {
   // 得分局
+  id: number
+  player1: User
+  player2: User
+  player3: User
+  player4: User
+  leftPoint: number
+  rightPoint: number
+  createBy: number
+  createTime: Date
+  updateBy: number
+  updateTime: Date
 }
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,17 +1,74 @@
 <template>
-  <view class="page-wrapper h-screen w-screen flex flex-col">
-    <view class="page-top h-20 w-100 bg-blue-500">
-      <view class="flex justify-center">搜索框</view>
-      <view class="flex justify-center">过滤器</view>
+  <view class="container">
+    <view class="search-bar">
+      <uv-input placeholder="请输入搜索内容" />
     </view>
-    <view class="page-main flex-grow w-100 bg-yellow-500">
-      <Item></Item>
+    <view class="dropdowns">
+      <uv-drop-down ref="dropDown" sign="dropDown_1" :defaultValue="[0, '0', 'all']">
+        <uv-drop-down-item name="order" type="2" label="综合排序" value="all"></uv-drop-down-item>
+        <uv-drop-down-item name="type" type="2" label="文档格式" value="0"></uv-drop-down-item>
+        <uv-drop-down-item name="vip_type" type="1" label="VIP文档" :value="0"></uv-drop-down-item>
+      </uv-drop-down>
+      <uv-drop-down-popup sign="dropDown_1">
+        <view style="width: 750rpx; height: 300rpx; background-color: #fff"></view>
+      </uv-drop-down-popup>
     </view>
+    <view class="list">
+      <uv-list>
+        <uv-list-item v-for="item in list" :key="item.id">
+          {{ item.name }}
+        </uv-list-item>
+      </uv-list>
+    </view>
+    <uni-bottom-nav>
+      <uni-bottom-nav-item icon="home" text="首页" />
+      <uni-bottom-nav-item icon="person" text="个人中心" />
+    </uni-bottom-nav>
   </view>
 </template>
 
-<script lang="ts" setup>
-// 系统首页，显示所有比赛
-import Item from './component/matchCard.vue'
-// 需要有搜索接口
+<script>
+import { ref, reactive } from 'vue'
+
+export default {
+  setup() {
+    const current = reactive(0)
+    const list = ref([
+      { id: 1, name: '项目1' },
+      { id: 2, name: '项目2' },
+      { id: 3, name: '项目3' },
+      // 更多项目...
+    ])
+    return {
+      list,
+    }
+  },
+}
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+  height: 100vh;
+}
+
+.search-bar {
+  width: 100%;
+  padding: 10px;
+}
+
+.dropdowns {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  padding: 10px;
+}
+
+.list {
+  width: 100%;
+  padding: 10px;
+}
+</style>
